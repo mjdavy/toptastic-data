f72910319f783ce07471cc7719395c907496ac16
Martin Davy <martin.davy@gmail.com>
2023-12-15 09:32:36 -0500

    added db and video id

--- toptastic-api.py
+++ toptastic-api.py
@@ -1,25 +1,34 @@
+import logging
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
-import json
+import sqlite3
 
 app = Flask(__name__)
 
+logging.basicConfig(filename='app.log', level=logging.INFO)
+
+def get_db_connection():
+    conn = sqlite3.connect('songs.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/api/songs/<date>', methods=['GET'])
 def get_songs(date):
+    conn = get_db_connection()
+    songs = conn.execute('SELECT * FROM songs WHERE date = ?', (date,)).fetchall()
+
+    if songs:
+        logging.info(f'Songs for date {date} found in database.')
+        return jsonify([dict(ix) for ix in songs])
+
     url = f'https://www.officialcharts.com/charts/singles-chart/{date}/7501/' 
     response = requests.get(url)
 
-    # Use the 'html.parser' to parse the page
     soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Find all the div tags with class 'description block'
     divs = soup.find_all('div', class_='description block')
 
-    # Create a list to store the song data
     songs = []
-
-    # Extract and print the song name, artist, and chart information
     for div in divs:
         song_name_tag = div.find('a', class_='chart-name font-bold inline-block')
         song_name_elements = song_name_tag.find_all('span')
@@ -29,22 +38,44 @@ def get_songs(date):
         lw = div.find('li', class_='movement px-2 py-1 rounded-md inline-block mr-1 sm:mr-2').get_text(strip=True).split(':')[1].replace(',', '')
         peak = div.find('li', class_='peak px-2 py-1 rounded-md inline-block mr-1 sm:mr-2').get_text(strip=True).split(':')[1].replace(',', '')
         weeks = div.find('li', class_='weeks px-2 py-1 rounded-md inline-block mr-1 sm:mr-2').get_text(strip=True).split(':')[1]
+        video_id = ''  # This will be populated by a separate process
 
-        # Create a dictionary for the song
         song = {
             're_new': re_new,
             'song_name': song_name,
             'artist': artist,
             'lw': lw,
             'peak': peak,
-            'weeks': weeks
+            'weeks': weeks,
+            'video_id': video_id
         }
         
-        # Add the song to the list
         songs.append(song)
 
-    # Return the list as a JSON response
+        conn.execute('''
+            INSERT INTO songs (date, re_new, song_name, artist, lw, peak, weeks, video_id)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (date, re_new, song_name, artist, lw, peak, weeks, video_id))
+
+    conn.commit()
+    conn.close()
+
+    logging.info(f'Songs for date {date} scraped from web and stored in database.')
     return jsonify(songs)
 
 if __name__ == '__main__':
+    conn = get_db_connection()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS songs (
+            date TEXT,
+            re_new TEXT,
+            song_name TEXT,
+            artist TEXT,
+            lw TEXT,
+            peak TEXT,
+            weeks TEXT,
+            video_id TEXT
+        )
+    ''')
+    conn.close()
     app.run(debug=True)
